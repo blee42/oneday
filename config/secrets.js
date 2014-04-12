9814df9063bf4149263583922b6862e7d2cb3072
bhuang92 <basilhuang2014@u.northwestern.edu>
2014-04-12 16:32:14 -0500

    updated fb login

--- config/secrets.js
+++ config/secrets.js
@@ -28,7 +28,7 @@ module.exports = {
   facebook: {
     clientID: process.env.FACEBOOK_ID || '698054533591478',
     clientSecret: process.env.FACEBOOK_SECRET || '240361e85307f77e5ac166263ed71543',
-    callbackURL: '/auth/facebook/callback',
+    callbackURL: 'http://127.0.0.1/auth/facebook/call',
     passReqToCallback: true
   },
 
